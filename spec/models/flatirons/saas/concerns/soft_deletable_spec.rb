0838453b676002c87ebc7453880acde2d81246c4
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-06-18 15:13:57 -0300

    fix: tests

--- spec/models/flatirons/saas/concerns/soft_deletable_spec.rb
+++ spec/models/flatirons/saas/concerns/soft_deletable_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require 'rails_helper'
 
 describe 'SoftDeletable' do
   with_model :BlogPost do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@
 
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
-require_relative 'dummy/config/environment'
+require File.expand_path('dummy/config/environment.rb',  __dir__)
 
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
@@ -10,6 +10,8 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'shoulda/matchers'
 require 'stripe_mock'
+require 'factory_bot_rails'
+require 'with_model'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -36,18 +38,20 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
+  config.extend WithModel
+end
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
   end
-end
-
-RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-  config.include FactoryBot::Syntax::Methods
-  config.infer_spec_type_from_file_location!
-  config.filter_rails_from_backtrace!
 end
