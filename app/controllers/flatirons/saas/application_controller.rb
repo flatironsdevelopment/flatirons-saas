0ff34aeecb56225080ca22b333ac3fa7bf9ac8b3
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-06-23 17:38:22 -0300

    refactor: added error methods

--- app/controllers/flatirons/saas/application_controller.rb
+++ app/controllers/flatirons/saas/application_controller.rb
@@ -2,7 +2,24 @@
 
 module Flatirons
   module Saas
-    class ApplicationController < ActionController::Base
+    class ApplicationController < ::ActionController::Base
+      respond_to :json
+      skip_forgery_protection
+
+      rescue_from ActiveRecord::RecordNotFound,       with: :not_found
+      rescue_from ActionController::ParameterMissing, with: :missing_param_error
+
+      def not_found(exception)
+        render_error exception.message, :not_found
+      end
+
+      def missing_param_error(exception)
+        render_error exception.message, :unprocessable_entity
+      end
+
+      def render_error(message, status, additional_data = {})
+        render json: additional_data.merge({ message: message }), status: status
+      end
     end
   end
 end
