22e308b6eb6a7b717aa75d67b782acccf2cafddc
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-06-23 17:39:30 -0300

    feat: update schema and swagger documentation

---
+++ spec/dummy/db/migrate/20220623194739_add_initial_data.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class AddInitialData < ActiveRecord::Migration[6.1]
+  def up
+    user = DummyUser.create(email: 'dummy.user@flatironsdevelopment.com', password: 'Password#123456')
+    subscription = Flatirons::Saas::Subscription.create(subscriptable: user, status: 'active', stripe_subscription_id: 'dummy_test')
+    pp subscription.errors
+  end
+
+  def down
+    DummyUser.delete_all
+    Flatirons::Saas::Subscription.delete_all
+  end
+end
