6ca1993f79f7d67aeb1fe3a6d094cc8708fdcf51
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-06-23 15:09:39 -0300

    feat: add devise to dummy project

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,4 +2,8 @@
 
 Rails.application.routes.draw do
   mount Flatirons::Saas::Engine => '/flatirons-saas'
+  devise_for :dummy_users
+  devise_scope :dummy_user do
+    subscription_for :dummy_users
+  end
 end
