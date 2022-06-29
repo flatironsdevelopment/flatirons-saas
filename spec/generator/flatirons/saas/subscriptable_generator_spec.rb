a585afb156844d328b748365ed50daf7555c6dcd
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-06-29 09:19:33 -0300

    fix: test

--- spec/generator/flatirons/saas/subscriptable_generator_spec.rb
+++ spec/generator/flatirons/saas/subscriptable_generator_spec.rb
@@ -3,20 +3,22 @@
 require 'rails_helper'
 require 'generator_spec'
 
-RSpec.describe Flatirons::Saas::Generators::SubscriptableGenerator, type: :generator do
-  destination File.expand_path('../tmp', __dir__)
-  arguments %w(test --test)
+module Flatirons::Saas::Generators
+  RSpec.describe SubscriptableGenerator, type: :generator do
+    destination File.expand_path('../tmp', __dir__)
+    arguments %w(test --test)
 
-  before(:all) do
-    prepare_destination
-    run_generator
-  end
+    before(:all) do
+      prepare_destination
+      run_generator
+    end
 
-  it 'creates a migration' do
-    assert_migration 'db/migrate/create_tests.rb'
-  end
+    it 'creates a migration' do
+      assert_migration 'db/migrate/create_tests.rb'
+    end
 
-  it 'creates a model' do
-    assert_file 'app/models/test.rb'
+    it 'creates a model' do
+      assert_file 'app/models/test.rb'
+    end
   end
 end
