a48a2116b0d59e3d8c662c7a50bc817bb7e708c5
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-04 10:18:44 -0300

    fix: stripe integration for tests

--- spec/support/dummy_user.rb
+++ spec/support/dummy_user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 shared_context 'dummy_user' do
+  include_context 'stripe'
   let!(:current_dummy_user) { DummyUser.create(email: 'flatirons-saas@flatironsdevelopment.com', password: 'Password#12345') }
 end
