b79937441092b03ec39aa9f373d548dd2e07624a
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-11 18:11:16 -0300

    feat: fix swagger

--- spec/requests/flatirons/saas/subscriptions_spec.rb
+++ spec/requests/flatirons/saas/subscriptions_spec.rb
@@ -38,11 +38,17 @@ describe '/dummy_users/subscriptions', type: :request do
         end
 
         after do |example|
-          example.metadata[:response][:content] = {
+          content = example.metadata[:response][:content] || {}
+          example_spec = {
             'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
+              examples: {
+                test_example: {
+                  value: JSON.parse(response.body, symbolize_names: true)
+                }
+              }
             }
           }
+          example.metadata[:response][:content] = content.deep_merge(example_spec)
         end
       end
     end
