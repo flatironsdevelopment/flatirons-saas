4b739746f6023f0558ac3c1d15b25a63328c1258
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-11 18:01:17 -0300

    feat: attach payment method

--- spec/requests/flatirons/saas/payment_methods_spec.rb
+++ spec/requests/flatirons/saas/payment_methods_spec.rb
@@ -10,8 +10,8 @@ describe '/dummy_users/payment_methods', type: :request do
   include_context 'dummy_user_with_payment_methods'
 
   path '/dummy_users/payment_methods' do
-    get('List dummy user payment methods') do
-      tags 'Subscription'
+    get('List payment methods') do
+      tags 'Payment Methods'
       description 'Lists current user payment methods.'
       consumes 'application/json'
       produces 'application/json'
@@ -48,5 +48,42 @@ describe '/dummy_users/payment_methods', type: :request do
         end
       end
     end
+
+    post('Attach payment method') do
+      tags 'Payment Methods'
+      description 'Lists current user payment methods.'
+      consumes 'application/json'
+      produces 'application/json'
+      security [bearer: []]
+      parameter name: :payment_method_params, in: :body, schema: {
+        type: :object,
+        properties: {
+          id: { type: :string },
+          set_as_default: { type: :boolean, nullable: true }
+        },
+        require: [:id]
+      }
+
+      let!(:new_payment_method) { Stripe::PaymentMethod.create(stripe_credit_card) }
+
+      response(200, 'successful') do
+        let(:payment_method_params) { { id: new_payment_method.id } }
+
+        run_test! do |response|
+          expect(response).to have_http_status(:ok)
+          expect(response.body).to include_json({ id: new_payment_method.id })
+        end
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+      end
+    end
+
+    let!(:second_payment_method_id) { Stripe::PaymentMethod.create(stripe_credit_card).id }
   end
 end
