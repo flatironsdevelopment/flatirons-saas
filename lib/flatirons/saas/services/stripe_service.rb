33e2290d874398c28f3442579745d0fad24ee687
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-11 10:31:17 -0300

    refactor: add set default payment method

--- lib/flatirons/saas/services/stripe_service.rb
+++ lib/flatirons/saas/services/stripe_service.rb
@@ -6,17 +6,32 @@ module Flatirons::Saas::Services
       Stripe::Customer.create extra_fields.merge({ name: name }), stripe_opts
     end
 
-    def destroy_customer(customer_id)
-      Stripe::Customer.delete customer_id, {}, stripe_opts
+    def destroy_customer(stripe_customer_id)
+      Stripe::Customer.delete stripe_customer_id, {}, stripe_opts
     end
 
-    def attach_payment_method(stripe_customer_id, payment_method_id)
+    def attach_payment_method(stripe_customer_id, payment_method_id, set_as_default: false)
       return if stripe_customer_id.nil? || payment_method_id.nil?
 
-      Stripe::PaymentMethod.attach(payment_method_id, { customer: stripe_customer_id })
+      set_default_payment_method(stripe_customer_id, payment_method_id) if set_as_default
+
+      Stripe::PaymentMethod.attach(payment_method_id, { customer: stripe_customer_id }, stripe_opts)
     end
 
     private
+
+    def set_default_payment_method(stripe_customer_id, payment_method_id)
+      settings = {
+        invoice_settings: {
+          default_payment_method: payment_method_id
+        }
+      }
+      Stripe::Customer.update(
+        stripe_customer_id,
+        settings,
+        stripe_opts
+      )
+    end
 
     def stripe_opts
       raise 'Stripe API key not configured' if Flatirons::Saas.stripe_api_key.nil?
