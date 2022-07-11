af379f210a8e9994017227105e488c8c21d96d71
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-11 16:24:20 -0300

    refactor: test organization

--- spec/rails/routes_set_spec.rb
+++ spec/rails/routes_set_spec.rb
@@ -12,90 +12,92 @@ describe 'RouteSet', type: :routing  do
       Flatirons::Saas::Rails::Product.mappings.delete(:user)
     end
 
-    context 'devise is not available' do
-      it 'should raise devise is not available' do
-        User.subscriptable
-        without_const('Devise') do
+    describe '#subscription_for' do
+      context 'devise is not available' do
+        it 'should raise devise is not available' do
+          User.subscriptable
+          without_const('Devise') do
+            expect do
+              routes = ActionDispatch::Routing::RouteSet.new
+              routes.draw do
+                subscription_for :users
+              end
+            end
+              .to raise_error 'Devise is not available, please include devise gem to get work.'
+          end
+        end
+      end
+
+      context 'devise is wrong configured with subscription_for' do
+        it 'should raise devise resource type error' do
+          User.subscriptable
+          expect do
+            routes = ActionDispatch::Routing::RouteSet.new
+            routes.draw do
+              devise_for :users, class_name: 'DummyUser'
+              subscription_for :users
+            end
+          end
+            .to raise_error 'Devise resource type [DummyUser] is not the same of subscription_for [User],'\
+            ' check your subscription_for/devise_for route configuration.'
+        end
+      end
+
+      context 'devise_for :users is not configured' do
+        it 'should raise devise for :users not found' do
+          User.subscriptable
           expect do
             routes = ActionDispatch::Routing::RouteSet.new
             routes.draw do
               subscription_for :users
             end
           end
-            .to raise_error 'Devise is not available, please include devise gem to get work.'
+            .to raise_error 'Devise for :users not found, please check your subscription_for/devise_for route configuration.'
         end
       end
-    end
 
-    context 'devise is wrong configured with subscription_for' do
-      it 'should raise devise resource type error' do
-        User.subscriptable
-        expect do
-          routes = ActionDispatch::Routing::RouteSet.new
-          routes.draw do
-            devise_for :users, class_name: 'DummyUser'
-            subscription_for :users
+      context 'user is not subscriptable' do
+        it 'should raise does not respond to \'subscriptable\' method' do
+          expect do
+            routes = ActionDispatch::Routing::RouteSet.new
+            routes.draw do
+              devise_for :users
+              subscription_for :users
+            end
           end
+            .to raise_error 'User does not respond to \'subscriptable\' method.'
         end
-          .to raise_error 'Devise resource type [DummyUser] is not the same of subscription_for [User],'\
-          ' check your subscription_for/devise_for route configuration.'
-      end
-    end
-
-    context 'devise_for :users is not configured' do
-      it 'should raise devise for :users not found' do
-        User.subscriptable
-        expect do
-          routes = ActionDispatch::Routing::RouteSet.new
-          routes.draw do
-            subscription_for :users
-          end
-        end
-          .to raise_error 'Devise for :users not found, please check your subscription_for/devise_for route configuration.'
-      end
-    end
-
-    context 'user is not subscriptable' do
-      it 'should raise does not respond to \'subscriptable\' method' do
-        expect do
-          routes = ActionDispatch::Routing::RouteSet.new
-          routes.draw do
-            devise_for :users
-            subscription_for :users
-          end
-        end
-          .to raise_error 'User does not respond to \'subscriptable\' method.'
-      end
-    end
-
-    context 'user is subscriptable' do
-      it 'should not raise error' do
-        User.subscriptable
-        expect do
-          routes = ActionDispatch::Routing::RouteSet.new
-          routes.draw do
-            devise_for :users
-            subscription_for :users
-          end
-        end
-          .not_to raise_error
       end
 
-      context 'routing' do
-        before(:each) { User.subscriptable }
-
-        routes do
-          route_set = ActionDispatch::Routing::RouteSet.new
-          route_set.draw do
-            devise_for :users
-            subscription_for :users
+      context 'user is subscriptable' do
+        it 'should not raise error' do
+          User.subscriptable
+          expect do
+            routes = ActionDispatch::Routing::RouteSet.new
+            routes.draw do
+              devise_for :users
+              subscription_for :users
+            end
           end
-          route_set
+            .not_to raise_error
         end
 
-        it 'should route to index' do
-          expect(get: '/users/subscriptions')
-            .to route_to(controller: 'flatirons/saas/subscriptions', action: 'index')
+        context 'routing' do
+          before(:each) { User.subscriptable }
+
+          routes do
+            route_set = ActionDispatch::Routing::RouteSet.new
+            route_set.draw do
+              devise_for :users
+              subscription_for :users
+            end
+            route_set
+          end
+
+          it 'should route to index' do
+            expect(get: '/users/subscriptions')
+              .to route_to(controller: 'flatirons/saas/subscriptions', action: 'index')
+          end
         end
       end
     end
