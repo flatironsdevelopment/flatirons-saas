18a227c979e69c3f2267eef2015b850d6e3c051f
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-12 16:12:21 -0300

    fix: remove wrong class declaration

--- spec/requests/flatirons/saas/products_spec.rb
+++ spec/requests/flatirons/saas/products_spec.rb
@@ -6,19 +6,8 @@ require 'rails_helper'
 describe '/dummy_users/products', type: :request do
   include_context 'dummy_user'
   include_context 'dummy_user_is_authenticated'
-
-  with_model 'Flatirons::Saas::Product' do
-    table do |t|
-      t.string :name
-      t.timestamps null: false
-    end
-
-    model do
-      productable
-    end
-  end
-
   include_context 'products_for_users'
+
   it 'lists all products' do
     FactoryBot.create_list(:product, 3)
     product_names = Flatirons::Saas::Product.all.pluck(:name).sort
