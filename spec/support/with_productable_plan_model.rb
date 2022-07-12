533bdd1b0ca3c216b762780449010f7f967d3cdc
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-12 16:03:01 -0300

    feat: add plan model for testing

---
+++ spec/support/with_productable_plan_model.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+shared_context 'with_productable_plan_model' do
+  include_context 'stripe'
+  with_model :Plan do
+    table do |t|
+      t.string :name
+      t.string :stripe_product_id
+      t.timestamps null: false
+    end
+
+    model do
+      productable delete_product_on_destroy: true
+    end
+  end
+end
