579deb14425ab8d22c2b5426402265015d7a69e7
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-12 17:13:49 -0300

    refactor: code clean up

--- app/controllers/flatirons/saas/application_controller.rb
+++ app/controllers/flatirons/saas/application_controller.rb
@@ -24,23 +24,26 @@ module Flatirons
 
       private
 
+      def mapping
+        @mapping ||= request.env['flatirons.saas.mapping']
+      end
+
       # Check if there is a signed in user before doing the action.
       #
       # If there is no signed in user, it will raise devise unauthorized error
       def authenticate!
-        @mapping ||= request.env['flatirons.saas.mapping']
-        authenticate_resource_for! @mapping[:symbol]
+        authenticate_resource_for! mapping[:symbol]
       end
 
       def authenticate_resource_for!(symbol, opts = {})
         opts[:scope] = symbol
         @current_resource = warden.authenticate!(opts)
-        raise_current_resource_mapping_error unless @current_resource.is_a? @mapping[:klass]
+        raise_current_resource_mapping_error unless @current_resource.is_a? mapping[:klass]
         @current_resource
       end
 
       def raise_current_resource_mapping_error
-        raise "Authenticated resource type [#{@current_resource.class}] is not the same of mapped [#{@mapping[:klass]}],"\
+        raise "Authenticated resource type [#{@current_resource.class}] is not the same of mapped [#{mapping[:klass]}],"\
         ' check your subscription_for/devise_for route configuration.'
       end
     end
