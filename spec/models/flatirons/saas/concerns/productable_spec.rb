f6bc0feb4a716d4d8f241055d01e7d60e6bf1c71
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-14 11:29:27 -0300

    feat: create_price/price tests and refactors

--- spec/models/flatirons/saas/concerns/productable_spec.rb
+++ spec/models/flatirons/saas/concerns/productable_spec.rb
@@ -26,8 +26,7 @@ describe Flatirons::Saas::Concerns::Productable do
 
       describe 'product creation' do
         it 'should create stripe product' do
-          service = instance_double(Flatirons::Saas::Services::StripeService)
-          allow(Flatirons::Saas::Services::StripeService).to receive(:new).and_return(service)
+          service = mock_stripe_service
           expect(service).to receive(:create_product).with(plan.name, {}).and_return(product)
 
           plan.save!
@@ -42,8 +41,7 @@ describe Flatirons::Saas::Concerns::Productable do
       let!(:stripe_product_id) { product.id }
 
       it 'should not create stripe product' do
-        service = instance_double(Flatirons::Saas::Services::StripeService)
-        allow(Flatirons::Saas::Services::StripeService).to receive(:new).and_return(service)
+        service = mock_stripe_service
         expect(service).to_not receive(:create_product)
 
         plan.save
@@ -81,8 +79,7 @@ describe Flatirons::Saas::Concerns::Productable do
       end
 
       it 'should run callbacks' do
-        service = instance_double(Flatirons::Saas::Services::StripeService)
-        allow(Flatirons::Saas::Services::StripeService).to receive(:new).and_return(service)
+        service = mock_stripe_service
         expect(service).to receive(:create_product).with(plan.name, {}).and_return(product)
 
         plan.save
@@ -105,8 +102,7 @@ describe Flatirons::Saas::Concerns::Productable do
       let!(:stripe_product_id) { nil }
 
       it 'should not destroy the stripe product' do
-        service = instance_double(Flatirons::Saas::Services::StripeService)
-        allow(Flatirons::Saas::Services::StripeService).to receive(:new).and_return(service)
+        service = mock_stripe_service
         expect(service).to_not receive(:destroy_product)
 
         plan.destroy
@@ -117,8 +113,7 @@ describe Flatirons::Saas::Concerns::Productable do
       let!(:stripe_product_id) { product.id }
 
       it 'should destroy the stripe product' do
-        service = instance_double(Flatirons::Saas::Services::StripeService)
-        allow(Flatirons::Saas::Services::StripeService).to receive(:new).and_return(service)
+        service = mock_stripe_service
         expect(service).to receive(:destroy_product).with(stripe_product_id)
 
         plan.destroy
@@ -141,8 +136,7 @@ describe Flatirons::Saas::Concerns::Productable do
       let!(:stripe_product_id) { nil }
 
       it 'should not destroy the stripe product' do
-        service = instance_double(Flatirons::Saas::Services::StripeService)
-        allow(Flatirons::Saas::Services::StripeService).to receive(:new).and_return(service)
+        service = mock_stripe_service
         allow(plan).to receive(:productable_options).and_return({})
 
         expect(service).to_not receive(:destroy_product)
@@ -151,8 +145,7 @@ describe Flatirons::Saas::Concerns::Productable do
       end
 
       it 'should not destroy the stripe product' do
-        service = instance_double(Flatirons::Saas::Services::StripeService)
-        allow(Flatirons::Saas::Services::StripeService).to receive(:new).and_return(service)
+        service = mock_stripe_service
         allow(plan).to receive(:productable_options).and_return({ delete_product_on_destroy: false })
 
         expect(service).to_not receive(:destroy_product)
@@ -180,8 +173,7 @@ describe Flatirons::Saas::Concerns::Productable do
       end
 
       it 'should run callbacks' do
-        service = instance_double(Flatirons::Saas::Services::StripeService)
-        allow(Flatirons::Saas::Services::StripeService).to receive(:new).and_return(service)
+        service = mock_stripe_service
         expect(service).to receive(:destroy_product).with(stripe_product_id)
 
         plan.destroy
@@ -191,6 +183,106 @@ describe Flatirons::Saas::Concerns::Productable do
         expect(callbacks).to have_received(:after_stripe_product_deletion)
 
         expect(plan.stripe_product_id).to eq(product.id)
+      end
+    end
+  end
+
+  describe 'create_price' do
+    let!(:product) { Stripe::Product.create({ name: 'plan_1' }) }
+    let!(:plan) { Plan.new(name: 'Flatirons', stripe_product_id: stripe_product_id) }
+    let!(:price) { Stripe::Price.create({ unit_amount: 4000, currency: 'usd', product: product.id, }) }
+
+    let!(:unit_amount) { 1099 }
+    let!(:currency) { 'usd' }
+    let!(:recurring_interval) { 'month' }
+
+    context 'when stripe product exist' do
+      let!(:stripe_product_id) { product.id }
+
+      it 'should create a price' do
+        service = mock_stripe_service
+
+        expect(service).to receive(:create_price).with(product_id: stripe_product_id, unit_amount: unit_amount,  currency: currency,
+                                                       recurring_interval: recurring_interval, extra_fields: {}).and_return(price)
+
+        new_price = plan.create_price(unit_amount: unit_amount, currency: currency, recurring_interval: recurring_interval)
+
+        expect(new_price).to be
+        expect(new_price.id).to eq price.id
+      end
+    end
+
+    context 'when stripe product does not exist' do
+      let!(:stripe_product_id) { nil }
+
+      it 'it should not create the price.' do
+        service = mock_stripe_service
+        expect(service).to_not receive(:create_price)
+
+        price = plan.create_price(unit_amount: unit_amount,  currency: currency, recurring_interval: recurring_interval)
+        expect(price).to_not be
+      end
+    end
+
+    context 'when stripe_product_id attribute does not exist' do
+      let!(:stripe_product_id) { nil }
+
+      it 'should raise stripe_product_id attribute not found.' do
+        service = mock_stripe_service
+        expect(service).to_not receive(:create_price)
+
+        allow(plan).to receive(:has_attribute?).with(:stripe_product_id).and_return false
+
+        expect do
+          plan.create_price(unit_amount: unit_amount, currency: currency,
+                            recurring_interval: recurring_interval)
+        end.to raise_error 'stripe_product_id attribute not found.'
+      end
+    end
+  end
+
+  describe 'prices' do
+    let!(:product) { Stripe::Product.create({ name: 'plan_1' }) }
+    let!(:plan) { Plan.new(name: 'Flatirons', stripe_product_id: stripe_product_id) }
+    let!(:price) { Stripe::Price.create({ unit_amount: 4000, currency: 'usd', product: product.id, }) }
+
+    context 'when stripe product exist' do
+      let!(:stripe_product_id) { product.id }
+
+      it 'should list prices' do
+        service = mock_stripe_service
+
+        expect(service).to receive(:list_prices).with(stripe_product_id).and_return([price])
+
+        prices = plan.prices
+
+        expect(prices.length).to eq 1
+        expect(prices[0]).to be
+        expect(prices[0].id).to eq price.id
+      end
+    end
+
+    context 'when stripe product does not exist' do
+      let!(:stripe_product_id) { nil }
+
+      it 'it should not list prices.' do
+        service = mock_stripe_service
+        expect(service).to_not receive(:list_prices)
+
+        price = plan.prices
+        expect(price).to_not be
+      end
+    end
+
+    context 'when stripe_product_id attribute does not exist' do
+      let!(:stripe_product_id) { nil }
+
+      it 'should raise stripe_product_id attribute not found.' do
+        service = mock_stripe_service
+        expect(service).to_not receive(:list_prices)
+
+        allow(plan).to receive(:has_attribute?).with(:stripe_product_id).and_return false
+        expect { plan.prices }.to raise_error 'stripe_product_id attribute not found.'
       end
     end
   end
