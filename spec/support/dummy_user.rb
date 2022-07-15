06945a03017aab3db69718758d38a4ef0435d79b
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-15 15:40:16 -0300

    fix: tests with subscription model

--- spec/support/dummy_user.rb
+++ spec/support/dummy_user.rb
@@ -2,5 +2,8 @@
 
 shared_context 'dummy_user' do
   include_context 'stripe'
-  let!(:current_dummy_user) { DummyUser.create(email: 'flatirons-saas@flatironsdevelopment.com', password: 'Password#12345') }
+  let!(:stripe_customer) { Stripe::Customer.create({ name: 'flatirons', source: stripe_helper.generate_card_token }) }
+  let!(:current_dummy_user) do
+    DummyUser.create(email: 'flatirons-saas@flatironsdevelopment.com', password: 'Password#12345', stripe_customer_id: stripe_customer.id)
+  end
 end
--- spec/support/dummy_user_with_subscription.rb
+++ spec/support/dummy_user_with_subscription.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 shared_context 'dummy_user_with_subscription' do
-  let!(:subscription) { FactoryBot.create(:subscription, subscriptable: current_dummy_user) }
+  let!(:product) { Flatirons::Saas::Product.create(name: 'Flatirons') }
+  let!(:stripe_price) { Stripe::Price.create({ unit_amount: 4000, currency: 'usd', product: product.stripe_product_id }) }
+  let!(:subscription) { FactoryBot.create(:subscription, subscriptable: current_dummy_user, product: product, stripe_price_id: stripe_price.id) }
 end
