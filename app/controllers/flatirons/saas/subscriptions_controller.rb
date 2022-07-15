3d0d4007f547ff188f1c9b71cf26e50b8db05941
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-15 16:22:52 -0300

    feat: create subscription

--- app/controllers/flatirons/saas/subscriptions_controller.rb
+++ app/controllers/flatirons/saas/subscriptions_controller.rb
@@ -3,10 +3,29 @@
 module Flatirons::Saas
   class SubscriptionsController < ApplicationController
     before_action :authenticate!
+    before_action :set_product, only: [:create]
 
     # GET /resource/subscriptions
     def index
       render json: @current_resource.subscriptions
+    end
+
+    # POST /resource/subscriptions
+    def create
+      subscription = @current_resource.subscriptions.create(product: @product, stripe_price_id: params[:stripe_price_id], status: :active)
+      if subscription.save
+        render json: subscription
+      else
+        render json: { errors: subscription.errors.full_messages.to_sentence }, status: :unprocessable_entity
+      end
+    end
+
+    def set_product
+      product_klass = mapping[:productable_klass]
+
+      @product = product_klass.find_by(id: params[:product_id])
+
+      render json: { message: 'Product not found' }, status: :not_found if @product.blank?
     end
   end
 end
--- spec/requests/flatirons/saas/subscriptions_spec.rb
+++ spec/requests/flatirons/saas/subscriptions_spec.rb
@@ -6,9 +6,120 @@ require 'rails_helper'
 describe '/dummy_users/subscriptions', type: :request do
   include_context 'dummy_user'
   include_context 'dummy_user_is_authenticated'
-  include_context 'dummy_user_with_subscription'
+
+  let!(:stripe_product) { Stripe::Product.create({ name: 'Beer' }) }
+  let!(:stripe_price) { Stripe::Price.create({  unit_amount: 4000, currency: 'usd', product: stripe_product.id }) }
+  let!(:product) { Flatirons::Saas::Product.create({ name: 'Beer', stripe_product_id: stripe_product.id }) }
 
   path '/dummy_users/subscriptions' do
+    post('Create a new subscription') do
+      tags 'Subscription'
+      description 'Lists current user payment methods.'
+      consumes 'application/json'
+      produces 'application/json'
+      security [bearer: []]
+      parameter name: :subscription_params, in: :body, schema: {
+        type: :object,
+        properties: {
+          product_id: { type: :integer },
+          stripe_price_id: { type: :string }
+        },
+        require: %i[product_id stripe_price_id]
+      }
+
+      context 'when product does not exist' do
+        response(404, 'not found') do
+          let!(:subscription_params) { { product_id: 10, stripe_price_id: nil } }
+
+          run_test! do |response|
+            expect(response).to have_http_status(:not_found)
+            error = JSON.parse(response.body)
+            expect(error['message']).to eq('Product not found')
+          end
+
+          after do |example|
+            content = example.metadata[:response][:content] || {}
+            example_spec = {
+              'application/json' => {
+                examples: {
+                  test_example: {
+                    value: JSON.parse(response.body, symbolize_names: true)
+                  }
+                }
+              }
+            }
+            example.metadata[:response][:content] = content.deep_merge(example_spec)
+          end
+        end
+      end
+
+      context 'when product exists' do
+        context 'when price is nil' do
+          response(422, 'unprocessable_entity') do
+            let!(:product) { Flatirons::Saas::Product.create(name: 'Flatirons') }
+            let!(:subscription_params) { { product_id: product.id, stripe_price_id: nil } }
+
+            run_test! do |response|
+              expect(response).to have_http_status(:unprocessable_entity)
+              error = JSON.parse(response.body)
+              expect(error['errors']).to eq('Stripe price can\'t be blank')
+            end
+
+            after do |example|
+              content = example.metadata[:response][:content] || {}
+              example_spec = {
+                'application/json' => {
+                  examples: {
+                    test_example: {
+                      value: JSON.parse(response.body, symbolize_names: true)
+                    }
+                  }
+                }
+              }
+              example.metadata[:response][:content] = content.deep_merge(example_spec)
+            end
+          end
+        end
+
+        context 'given a valid price' do
+          response(200, 'successful') do
+            let!(:subscription_params) { { product_id: product.id, stripe_price_id: stripe_price.id } }
+
+            run_test! do |response|
+              expect(response).to have_http_status(:ok)
+              subscription = JSON.parse(response.body)
+              expect(subscription['id']).to_not be_nil
+              expect(subscription['stripe_subscription_id']).to_not be_nil
+              expect(subscription['stripe_price_id']).to eq stripe_price.id
+              expect(subscription['status']).to eq 'active'
+              expect(subscription['subscriptable_type']).to eq 'DummyUser'
+              expect(subscription['subscriptable_id']).to eq current_dummy_user.id
+              expect(subscription['product_type']).to eq 'Flatirons::Saas::Product'
+              expect(subscription['product_id']).to eq product.id
+              expect(subscription['deleted_at']).to be_nil
+              expect(subscription['canceled_at']).to be_nil
+              expect(subscription['created_at']).to_not be_nil
+              expect(subscription['updated_at']).to_not be_nil
+            end
+
+            after do |example|
+              content = example.metadata[:response][:content] || {}
+              example_spec = {
+                'application/json' => {
+                  examples: {
+                    test_example: {
+                      value: JSON.parse(response.body, symbolize_names: true)
+                    }
+                  }
+                }
+              }
+              example.metadata[:response][:content] = content.deep_merge(example_spec)
+            end
+          end
+        end
+      end
+    end
+
     get('List dummy user subscriptions') do
       tags 'Subscription'
       description 'Lists current user subscriptions.'
@@ -16,39 +127,46 @@ describe '/dummy_users/subscriptions', type: :request do
       produces 'application/json'
       security [bearer: []]
 
-      response(200, 'successful') do
-        schema type: :array,
-               items: {
-                 type: :object,
-                 properties: {
-                   id: { type: :integer },
-                   stripe_subscription_id: { type: :string },
-                   subscriptable_type: { type: :string },
-                   subscriptable_id: { type: :integer },
-                   status: { type: :string },
-                   deleted_at: {  type: :string, nullable: true },
-                   canceled_at: { type: :string, nullable: true }
-                 },
-               }
+      context 'given one subscription' do
+        let!(:subscription) { FactoryBot.create(:subscription, subscriptable: current_dummy_user, product: product, stripe_price_id: stripe_price.id) }
 
-        run_test! do |response|
-          expect(response).to have_http_status(:ok)
-          expect(response.body).to include_json([{ id: subscription.id, stripe_subscription_id: subscription.stripe_subscription_id,
-                                                   status: subscription.status }])
-        end
+        response(200, 'successful') do
+          schema type: :array,
+                 items: {
+                   type: :object,
+                   properties: {
+                     id: { type: :integer },
+                     stripe_subscription_id: { type: :string },
+                     stripe_price_id: { type: :string },
+                     subscriptable_type: { type: :string },
+                     subscriptable_id: { type: :integer },
+                     product_type: { type: :string },
+                     product_id: { type: :integer },
+                     status: { type: :string },
+                     deleted_at: {  type: :string, nullable: true },
+                     canceled_at: { type: :string, nullable: true }
+                   },
+                 }
 
-        after do |example|
-          content = example.metadata[:response][:content] || {}
-          example_spec = {
-            'application/json' => {
-              examples: {
-                test_example: {
-                  value: JSON.parse(response.body, symbolize_names: true)
+          run_test! do |response|
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to include_json([{ id: subscription.id, stripe_subscription_id: subscription.stripe_subscription_id,
+                                                     status: subscription.status }])
+          end
+
+          after do |example|
+            content = example.metadata[:response][:content] || {}
+            example_spec = {
+              'application/json' => {
+                examples: {
+                  test_example: {
+                    value: JSON.parse(response.body, symbolize_names: true)
+                  }
                 }
               }
             }
-          }
-          example.metadata[:response][:content] = content.deep_merge(example_spec)
+            example.metadata[:response][:content] = content.deep_merge(example_spec)
+          end
         end
       end
     end
