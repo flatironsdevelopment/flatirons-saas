5ed318a869fd8b6feb6abdf0abbac2d76808a2ab
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-15 15:39:10 -0300

    feat: create subscription on stripe

--- db/migrate/20220715150706_create_flatirons_saas_subscriptions.rb
+++ db/migrate/20220715150706_create_flatirons_saas_subscriptions.rb
@@ -2,13 +2,16 @@ class CreateFlatironsSaasSubscriptions < ActiveRecord::Migration[6.1]
   def change
     create_table :flatirons_saas_subscriptions do |t|
       t.string :stripe_subscription_id, null: false
+      t.string :stripe_price_id, null: false
       t.string :status, null: false
       t.references :subscriptable, polymorphic: true
+      t.references :product, polymorphic: true
       t.timestamp :deleted_at
       t.timestamp :canceled_at
       t.timestamps
     end
     add_index :flatirons_saas_subscriptions, :stripe_subscription_id, unique: true
+    add_index :flatirons_saas_subscriptions, :stripe_price_id
     add_index :flatirons_saas_subscriptions, :status
     add_index :flatirons_saas_subscriptions, :deleted_at
   end
