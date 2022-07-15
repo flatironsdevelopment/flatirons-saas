88ee71ff2193e5899910fe1a2112cb20fff68d5d
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-07-15 11:09:27 -0300

    fix: replace

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,7 +18,7 @@ Dir['spec/support/**/*.rb'].sort.each { |f| require File.expand_path(f) }
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs.to_not be_nil
+# in _spec.rb will both be required and run as specs, causing the specs to be
 # run twice. It is recommended that you do not name files matching this glob to
 # end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
